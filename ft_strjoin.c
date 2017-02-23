b48b171270f322b43f84c47b27b8f90c8b03c79e
Safaiya SUMODHEE <ssumodhe@e2r6p15.42.fr>
2017-02-23 14:29:20 +0100

    strjoin changed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ssumodhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 12:18:04 by ssumodhe          #+#    #+#             */
-/*   Updated: 2016/11/26 16:16:00 by ssumodhe         ###   ########.fr       */
+/*   Updated: 2017/02/20 13:11:19 by ssumodhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,25 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		m;
 	char	*join;
 
-	if ((s1 == 0) || (s2 == 0))
+	if ((s1 == 0) && (s2 == 0))
 		return (NULL);
-	n = ft_strlen(s1);
-	m = ft_strlen(s2);
-	if ((join = (char *)malloc(sizeof(char) * (n + m + 1))) == NULL)
-		return (NULL);
-	n = 0;
-	while (s1[n] != '\0')
+	else if ((s1 != NULL) && (s2 == NULL))
+		return (ft_strdup(s1));
+	else if ((s1 == NULL) && (s2 != NULL))
+		return (ft_strdup(s2));
+	else
 	{
-		join[n] = s1[n];
-		n++;
+		n = ft_strlen(s1);
+		m = ft_strlen(s2);
+		if ((join = (char *)malloc(sizeof(char) * (n + m + 1))) == NULL)
+			return (NULL);
+		n = -1;
+		while (s1[++n] != '\0')
+			join[n] = s1[n];
+		m = 0;
+		while (s2[m] != '\0')
+			join[n++] = s2[m++];
+		join[n] = '\0';
 	}
-	m = 0;
-	while (s2[m] != '\0')
-	{
-		join[n] = s2[m];
-		n++;
-		m++;
-	}
-	join[n] = '\0';
 	return (join);
 }
